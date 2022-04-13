ceef6756d42830779893bfc908756cd758fd717e
adeelkbw <adeelnasirkbw@gmail.com>
2022-04-13 23:20:11 +0500

    access and get the 10 ether acounts from ganache-cli local network

--- test/Inbox.text.js
+++ test/Inbox.text.js
@@ -1,39 +1,26 @@
 const assert = require('assert');
-// const ganache = require('ganache-cli');
+const ganache = require('ganache-cli');
 const Web3 = require('web3');
 
-// const web3 = new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());
 
 
 
-
-
-class Car {
-    park() {
-        return "Parked"
-    }
-
-    drive() {
-        return "stopped"
-    }
-}
-
-//beforeeach run before execuation on=f it statement
-// if we have line of code in multiple it statements then we use beforeEach
-let car;
 beforeEach(() => {
-    car = new Car();
+    web3.eth.getAccounts()
+        .then((res) => {
+            console.log(res);
+        }).catch((err) => {
+            console.log(err);
+        })
 })
 
+describe('Inbox', () => {
+    it('deploy contract', () => {
 
-//a describe contains multiple it statements
-describe('Car', () => {
-    it('can park', () => {
-        // const car = new Car();
-        assert.equal(car.park(), 'Parked')
-    });
-    it('can stop', () => {
-        // const car = new Car();
-        assert.equal(car.drive(), 'stopped')
     })
 })
+
+
+
+
